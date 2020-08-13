6492cd3f1aeb5ab177ea1995998e4adbfe157279
idsherma <idsherma@gmail.com>
2020-08-12 23:37:18 -0400

    Node Callback Hell and Promises

--- operations.js
+++ operations.js
@@ -2,41 +2,22 @@ const assert = require('assert').strict;
 
 //export 4 methods
 
-exports.insertDocument = (db, document, collection, callback) => {
-    //`coll` = short for collection
-    //can use this `coll` constant to interact with a specific collection
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
-
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection);
-
-    //$set = allows us to change the value of a selected MongoDB field with a new value or set of values
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
